798553d64ab04f1fc6b20fede30ff4b0740310f1
konafx <afxlieonon@gmail.com>
2022-04-19 21:44:15 +0900

    change replaceText to firestored
    - store replaceStr2d to firestore
    - change replaceText

--- bq/csv_test.go
+++ bq/csv_test.go
@@ -17,35 +17,6 @@ func TestReplaceTimeUnit(t *testing.T) {
 	}
 }
 
-func TestReplaceLines(t *testing.T) {
-	in_string := []string{
-		"Om(19809m)",
-		"320(486回)",
-		"おなか押しにみひねり",
-		"バンザイスクワフット",
-		"- 5回(5回)",
-		"2 m(19809m)",
-		"Im(801m)",
-	}
-	wants := []string{
-		"0m(19809m)",
-		"32回(486回)",
-		"おなか押しこみひねり",
-		"バンザイスクワット",
-		"5回(5回)",
-		"2m(19809m)",
-		"1m(801m)",
-	}
-	out_string := replaceLines(in_string)
-	for idx, out := range out_string {
-		want := wants[idx]
-		if out != want {
-			err := errors.Errorf("Fail.\n[out ]: %s\n[want]: %s", out, want)
-			t.Error(err)
-		}
-	}
-}
-
 func TestCreateCsvDetails(t *testing.T) {
 	tweetInfo := TweetInfo{
 		TwitterId: "test",
--- firestore/firestore.go
+++ firestore/firestore.go
@@ -41,3 +41,35 @@ func GetParticipants(ctx context.Context, projectID string) (participants []Part
 
 	return
 }
+
+type Replacer struct{
+	Before string `firestore:"before"`
+	After string `firestore:"after"`
+}
+
+func GetReplacers(ctx context.Context, projectID string) (replacers []Replacer, err error) {
+	client, err := firestore.NewClient(ctx, projectID)
+	if err != nil {
+		return
+	}
+
+	iter := client.Collection("rfa-replacers").Documents(ctx)
+	for {
+		var replacer Replacer
+
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return replacers, err
+		}
+
+		if err = doc.DataTo(&replacer); err != nil {
+			return replacers, err
+		}
+		replacers = append(replacers, replacer)
+	}
+
+	return
+}
